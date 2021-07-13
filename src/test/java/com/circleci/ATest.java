09c61582a2c7e53c0665b9b0fcea595625cba83b
Patrick Shields <patrick.m.shields@gmail.com>
2021-07-13 12:04:06 -0400

    BLAH

--- src/test/java/com/circleci/ATest.java
+++ src/test/java/com/circleci/ATest.java
@@ -2,11 +2,13 @@ package com.circleci;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ATest {
 
   @Test
   public void test() {
-
+    assertEquals(1, 1);
   }
 
 }
