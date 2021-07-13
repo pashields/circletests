9e50e463f48aad7fbee77043e5617c1f9aafa20d
Patrick Shields <patrick.m.shields@gmail.com>
2021-07-13 11:51:41 -0400

    Add circle test script

---
+++ circle_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git fetch origin
+b64diff=$(git diff origin/master | base64 -e | tr -d '\n\r')
+curl -vvv -X "POST" "https://circleci.com/api/v2/project/gh/pashields/circletests/pipeline" \
+     -H "Circle-Token: ${CIRCLE_TOKEN}" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "parameters": {
+    "b64diff": "'$b64diff'"
+  },
+  "branch": "master"
+}'
