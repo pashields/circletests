3ffdbe5e683fa2519f527674ed19f8142f415a2f
Patrick Shields <patrick.m.shields@gmail.com>
2021-07-15 10:49:29 -0400

    Bleh

--- circle_test.sh
+++ circle_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git fetch origin
 b64diff=$(git diff origin/master | base64 -e | tr -d '\n\r')
-curl -vvv -X "POST" "https://circleci.com/api/v2/project/gh/pashields/circletests/pipeline" \
+curl -X "POST" "https://circleci.com/api/v2/project/gh/pashields/circletests/pipeline" \
      -H "Circle-Token: ${CIRCLE_TOKEN}" \
      -H 'Content-Type: application/json; charset=utf-8' \
      -d $'{
